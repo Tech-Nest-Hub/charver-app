78319959f712f4ab6c536a1e9891902ee6014694
Ghost9841 <asubedi778@gmail.com>
2025-07-02 13:48:58 +0545

    Learned how to add routes

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,12 @@
+
+import { Link } from 'expo-router';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function HomeScreen() {
   return (
     <View style={styles.container}>
       <Text>Home</Text>
+      <Link href="./settings"> Settings</Link>
     </View>
   );
 }
--- app/settings/index.tsx
+++ app/settings/index.tsx
@@ -1,15 +1,12 @@
 import React, { Component } from 'react'
 import { Text, View } from 'react-native'
 
-export default class index extends Component {
+export default class Settings extends Component {
   render() {
     return (
       <View>
-        <Text> Settings  </Text>
+        <Text> Settings </Text>
       </View>
     )
   }
 }
-
-
-
